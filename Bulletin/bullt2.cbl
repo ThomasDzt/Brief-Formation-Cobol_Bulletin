3e3d9c4dabf4b18e7e209a4e682bc563e267fb4d
ThomasDzt <thomas.durizot@outlook.fr>
2025-05-24 14:33:17 +0200

    bullt2.cbl modified

--- Bulletin/bullt2.cbl
+++ Bulletin/bullt2.cbl
@@ -38,11 +38,11 @@
            FILE STATUS IS F-STATUT-ENTREE.     
                    
        
-           SELECT FICHIER-SORTIE
-           ASSIGN TO 'output.dat'
-           ACCESS MODE IS SEQUENTIAL
-           ORGANIZATION IS LINE SEQUENTIAL
-           FILE STATUS IS F-OUTPUT-STATUS.     
+      *    SELECT FICHIER-SORTIE
+      *    ASSIGN TO 'output.dat'
+      *    ACCESS MODE IS SEQUENTIAL
+      *    ORGANIZATION IS LINE SEQUENTIAL
+      *    FILE STATUS IS F-OUTPUT-STATUS.     
 
 
 
@@ -63,27 +63,14 @@
       *    01  F-ENTREE-10      PIC X(10).
       *    01  F-ENTREE-100     PIC X(100).
            01  F-ENTREE-1000    PIC X(1000).
+              
        
-      *Données correspondant aux lignes pour chaque étudiant (clé, nom, prénom, âge)
-      *    01  F-LIGNE-ETUDIANT.
-      *        03 F-ETU-CLE        PIC 9(02).       
-      *        03 F-NOM            PIC X(07).       
-      *        03 F-PRENOM         PIC X(06).       
-      *        03 F-AGE            PIC 9(02).       
-       
-      *Données correspondant aux lignes pour chaque cours (clé, matière, coefficient, note) 
-      *    01  F-LIGNE-COURS.
-      *        03 F-C-CLE          PIC 9(02).       
-      *        03 F-MATIERE        PIC X(21).       
-      *        03 F-COEF           PIC 9,9.       
-      *        03 F-NOTE           PIC 99,99.       
-       
-       FD  FICHIER-SORTIE
-           RECORD CONTAINS 100 CHARACTERS
-           RECORDING MODE IS F.   
+      *FD  FICHIER-SORTIE
+      *    RECORD CONTAINS 100 CHARACTERS
+      *    RECORDING MODE IS F.   
 
-           01 F-SORTIE        PIC X(100).
-           
+      *    01 F-SORTIE        PIC X(100).
+      *    
          
 
       *----------------------------------------------------------------- 
@@ -123,57 +110,46 @@
                  15 WS-COEF       PIC 9V9.
                  15 WS-NOTE       PIC 99V99.        
 
-      * Création d'une variable permettant de stocker le calcul de note * coeff pour chaque matière
-                 15 WS-NOTECOEF   PIC 99V99.
+      *Création d'une variable permettant de stocker le calcul de note * coeff pour chaque matière
+                 15 WS-NOTE-POND   PIC 99V999.
+
+      *Création d'une variable permettant de stocker la moyenne pour chaque matière             
+                 15 WS-MOY-MAT    PIC 99V99.
        
-       
-      *Création d'une variable permettant de stocker temporairement
-      * le calcul de somme pondérée pour chaque matière     
-      
-       01 WS-SOM-TEMPO  PIC 999V99.
-
-      *Création d'un tableau pour récupérer les matières 
-       01 WS-TABLEAU-MATIERE.
-           05 WS-NBRE-MAT         PIC 9(03)        VALUE 0.
-           05 WS-MATIERE-UNIQ     OCCURS 999 TIMES.
-               10 WS-NOM-MAT-UNIQ PIC X(21). 
-
-
-       01 WS-TEMPO                PIC X(21). 
-
-       01 WS-TROUVE               PIC X            VALUE "N". *> indicateur de recherche
-
-      *Création de variables d'en-tête pour l'affichage 
-       01 WS-ENTETE-NOM           PIC X(07)       VALUE "Nom".
-       01 WS-ENTETE-PRENOM        PIC X(08)       VALUE "Prenom".
-       01 WS-ENTETE-AGE           PIC X(03)       VALUE "Age".    
-       01 WS-ENTETE-MAT           PIC X(21)       VALUE "Matiere".     
-       01 WS-ENTETE-COEF          PIC X(05)       VALUE "Coef". 
-       01 WS-ENTETE-NOTE          PIC X(04)       VALUE "Note". 
-       01 WS-ENTETE-MOYENNE       PIC X(07)       VALUE "Moyenne". 
-       01 WS-ETOILE               PIC X(31)       VALUE ALL "*".
-       01 WS-TIRET                PIC X(31)       VALUE ALL "-".
-
-       
-      *Création d'une variable permettant de stocker le total des coeff 
-       01 WS-TOT-COEF             PIC 9V9. 
-      
+      *Création d'une variable permettant de stocker le calcul de moyenne * coeff pour chaque matière
+                 15 WS-MOY-MAT-POND   PIC 99V999. 
 
       *Création d'une variable permettant de stocker
-      * le calcul de somme pondérée pour chaque matière   
-       01 WS-SOMME               PIC 999V99.
+      *les calculs de somme pondérée des notes par étudiant pour toutes les matières
+      *et de somme des notes par matière pour tous les étudiants  
+       01 WS-SOMME                PIC 999V999.
+
+      *Création d'une variable permettant de stocker le total des coeff 
+       01 WS-TOT-COEF             PIC 9V9. 
+       
+      *Création d'une variable pour stocker la moyenne de classe  
+       01 WS-MOYENNE-CLASSE       PIC 99V99.
+
+      *Création de variables d'en-tête pour l'affichage 
+       01 WS-ENTETE-NOM           PIC X(07)      VALUE "Nom".
+       01 WS-ENTETE-PRENOM        PIC X(08)      VALUE "Prenom".
+       01 WS-ENTETE-AGE           PIC X(03)      VALUE "Age".    
+       01 WS-ENTETE-MAT           PIC X(21)      VALUE "Matiere".     
+       01 WS-ENTETE-COEF          PIC X(05)      VALUE "Coef". 
+       01 WS-ENTETE-NOTE          PIC X(04)      VALUE "Note". 
+       01 WS-ENTETE-MOYENNE       PIC X(07)      VALUE "Moyenne". 
+       01 WS-ENTETE-MOY-CLS       PIC X(16)      VALUE "Moyenne classe". 
+       01 WS-ETOILE               PIC X(31)      VALUE ALL "*".
+       01 WS-TIRET                PIC X(31)      VALUE ALL "-".
+
        
 
       *Création d'index pour parcourir le tableau selon les dimensions
        77 WS-IDX-ETUD            PIC 9(03)        VALUE 0.      
        77 WS-IDX-COURS           PIC 9(03)        VALUE 0.
-
-      *Création d'index pour parcourir le tableau de collecte des matières
-      *uniques
-       77 WS-IDX-MAT         PIC 9(03)        VALUE 0. 
       
 
-
+      *01 WS-DUMMY           PIC X.
       ****************************************************************** 
       *                      PROCEDURE DIVISION                        *
       ****************************************************************** 
@@ -181,22 +157,14 @@
 
        PERFORM 0100-LECTURE-DEBUT
           THRU 0100-LECTURE-FIN.
+ 
+       PERFORM 0200-MOYENNE-ETU-DEBUT
+          THRU 0200-MOYENNE-ETU-FIN.
+ 
+       PERFORM 0300-MOYENNE-MAT-DEBUT
+          THRU 0300-MOYENNE-MAT-FIN.  
 
-       PERFORM 0200-AFFICHE-DEBUT
-          THRU 0200-AFFICHE-FIN.
-
-       PERFORM 0300-MOYENNE-ETU-DEBUT
-          THRU 0300-MOYENNE-ETU-FIN.
-
-       PERFORM 0400-AFFICHE2-DEBUT
-          THRU 0400-AFFICHE2-FIN.
-
-      *PERFORM 0500-MOYENNE-MAT-DEBUT
-      *   THRU 0500-MOYENNE-MAT-FIN.  
-
-      *PERFORM 0510-RECUP-MAT-DEBUT
-      *   THRU 0510-RECUP-MAT-FIN.
-
+       
        STOP RUN. 
 
 
@@ -233,7 +201,10 @@
        CLOSE FICHIER-ENTREE.
        DISPLAY "Fermeture du fichier.".
 
-       
+
+       PERFORM 0120-AFFICHE-DEBUT
+          THRU 0120-AFFICHE-FIN.
+
        0100-LECTURE-FIN.
        EXIT.
 
@@ -278,7 +249,7 @@
        EXIT.
 
       *-----------------------------------------------------------------
-       0200-AFFICHE-DEBUT.
+       0120-AFFICHE-DEBUT.
 
        DISPLAY "Affichage du fichier stocké :".
 
@@ -319,11 +290,11 @@
            END-PERFORM 
        END-PERFORM.
 
-       0200-AFFICHE-FIN.
+       0120-AFFICHE-FIN.
        EXIT.
 
       *-----------------------------------------------------------------
-       0300-MOYENNE-ETU-DEBUT.
+       0200-MOYENNE-ETU-DEBUT.
       
        PERFORM VARYING WS-IDX-ETUD FROM 1 BY 1 
                UNTIL WS-IDX-ETUD > WS-NBRE-ETUDIANT
@@ -332,19 +303,19 @@
            
         MOVE 0 TO WS-SOMME 
         MOVE 0 TO WS-TOT-COEF
-        MOVE 0 TO WS-SOM-TEMPO
+      
         PERFORM VARYING WS-IDX-COURS FROM 1 BY 1 
             UNTIL WS-IDX-COURS > WS-NBRE-COURS 
 
            
-           COMPUTE WS-NOTECOEF(WS-IDX-ETUD,WS-IDX-COURS) =
+           COMPUTE WS-NOTE-POND(WS-IDX-ETUD,WS-IDX-COURS) =
                    WS-COEF(WS-IDX-ETUD,WS-IDX-COURS) * 
                    WS-NOTE(WS-IDX-ETUD,WS-IDX-COURS)
            
            
 
-           ADD WS-NOTECOEF(WS-IDX-ETUD,WS-IDX-COURS)
-           TO  WS-SOM-TEMPO
+           ADD WS-NOTE-POND(WS-IDX-ETUD,WS-IDX-COURS)
+           TO  WS-SOMME
 
            
            ADD WS-COEF(WS-IDX-ETUD,WS-IDX-COURS)
@@ -352,21 +323,25 @@
            
 
         END-PERFORM 
-        MOVE WS-SOM-TEMPO
-        TO   WS-SOMME 
+     
+       
         COMPUTE WS-MOYENNE(WS-IDX-ETUD) ROUNDED =
                 WS-SOMME / WS-TOT-COEF
 
                
        END-PERFORM.
       
-       0300-MOYENNE-ETU-FIN.
+       PERFORM 0210-AFFICHE-MOY-ETUD-DEBUT
+          THRU 0210-AFFICHE-MOY-ETUD-FIN.
+
+
+       0200-MOYENNE-ETU-FIN.
        EXIT.
 
       *-----------------------------------------------------------------
 
-       0400-AFFICHE2-DEBUT.
-       DISPLAY "Affichage des moyennes :".
+       0210-AFFICHE-MOY-ETUD-DEBUT.
+       DISPLAY "Affichage des moyennes par étudiant :".
 
        PERFORM VARYING WS-IDX-ETUD FROM 1 BY 1 
                UNTIL WS-IDX-ETUD > WS-NBRE-ETUDIANT
@@ -386,119 +361,109 @@
                        
            DISPLAY WS-ETOILE
 
-               DISPLAY WS-ENTETE-MOYENNE
-               DISPLAY WS-MOYENNE(WS-IDX-ETUD)
-                       
-               
-
-               DISPLAY WS-TIRET
+           DISPLAY WS-ENTETE-MOYENNE
+           DISPLAY WS-MOYENNE(WS-IDX-ETUD)
+            
+           DISPLAY WS-TIRET
       
        END-PERFORM.
        
-       0400-AFFICHE2-FIN.
+       0210-AFFICHE-MOY-ETUD-FIN.
        EXIT.
 
       *-----------------------------------------------------------------
-      *0500-MOYENNE-MAT-DEBUT.
-
-      *PERFORM 0510-RECUP-MAT-DEBUT
-      *   THRU 0510-RECUP-MAT-FIN. 
-
-      *PERFORM VARYING WS-IDX-MAT FROM 1 BY 1 
-      *                UNTIL WS-IDX-MAT > WS-NBRE-MAT 
-
+       0300-MOYENNE-MAT-DEBUT.
+      
        
+       PERFORM VARYING WS-IDX-COURS FROM 1 BY 1 
+               UNTIL WS-IDX-COURS > WS-NBRE-COURS
            
-          
-      * MOVE 0 TO WS-SOMME 
-      * MOVE 0 TO WS-TOT-COEF
-      * MOVE 0 TO WS-SOM-TEMPO
+           MOVE 0 TO WS-SOMME
+           PERFORM VARYING WS-IDX-ETUD FROM 1 BY 1 
+                   UNTIL WS-IDX-ETUD > WS-NBRE-ETUDIANT
 
-      * PERFORM VARYING WS-IDX-ETUD FROM 1 BY 1 
-      *     UNTIL WS-IDX-ETUD > WS-NBRE-ETUDIANT
-
-      *    PERFORM VARYING WS-IDX-COURS FROM 1 BY 1 
-      *     UNTIL WS-IDX-COURS > WS-NBRE-COURS 
-      *    COMPUTE WS-NOTECOEF(WS-IDX-ETUD,WS-IDX-COURS) =
-      *            WS-COEF(WS-IDX-ETUD,WS-IDX-COURS) * 
-      *            WS-NOTE(WS-IDX-ETUD,WS-IDX-COURS)
-      *    
-      *    
-
-      *    ADD WS-NOTECOEF(WS-IDX-ETUD,WS-IDX-COURS)
-      *    TO  WS-SOM-TEMPO
-
-      *    
-      *    ADD WS-COEF(WS-IDX-ETUD,WS-IDX-COURS)
-      *    TO  WS-TOT-COEF 
-      *    
-      *    END-PERFORM 
-      * END-PERFORM 
-
-      * MOVE WS-SOM-TEMPO
-      * TO   WS-SOMME 
-      * COMPUTE WS-MOYENNE(WS-IDX-ETUD) ROUNDED  =
-      *         WS-SOMME / WS-TOT-COEF
-      * 
-      *        
-      *END-PERFORM.
+      *    DISPLAY WS-NOTE(WS-IDX-ETUD, WS-IDX-COURS)
+      *    ACCEPT WS-DUMMY
+               ADD WS-NOTE(WS-IDX-ETUD,WS-IDX-COURS)
+               TO  WS-SOMME
+      *        DISPLAY "somme/matiere" WS-SOM-NOTE-MAT
+           END-PERFORM     
+           COMPUTE WS-MOY-MAT(WS-IDX-ETUD,WS-IDX-COURS) ROUNDED = 
+                   WS-SOMME / WS-NBRE-ETUDIANT
+      *    DISPLAY "moy/mat" WS-MOY-MAT(WS-IDX-ETUD,WS-IDX-COURS)
+            
+       END-PERFORM.
 
 
-      *0500-MOYENNE-MAT-FIN.
-      *EXIT.
+       PERFORM 0310-MOYENNE-CLASSE-DEBUT
+          THRU 0310-MOYENNE-CLASSE-FIN.
+
+       PERFORM 0320-AFFICHE-MOY-MAT-DEBUT
+          THRU 0320-AFFICHE-MOY-MAT-FIN.
+
+       0300-MOYENNE-MAT-FIN.
+       EXIT.
        
-      *-----------------------------------------------------------------
-      *0510-RECUP-MAT-DEBUT.
-      *PERFORM VARYING WS-IDX-ETUD FROM 1 BY 1 
-      *        UNTIL WS-IDX-ETUD > WS-NBRE-ETUDIANT
-      *    
-      *    PERFORM VARYING WS-IDX-COURS FROM 1 BY 1 
-      *            UNTIL   WS-IDX-COURS > WS-NBRE-COURS
-
-      *Récupération de chaque matière dans le tableau tampon dans une variable temporaire     
-      *     MOVE WS-MATIERE(WS-IDX-ETUD, WS-IDX-COURS)
-      *     TO WS-TEMPO 
-
-      *Initialisation de la première valeur du tableau tampon 
-      *     IF WS-NBRE-MAT = 0 
-      *        ADD 1 TO WS-NBRE-MAT
-      *        MOVE WS-TEMPO TO WS-NOM-MAT-UNIQ(WS-NBRE-MAT)
-
-      *     ELSE 
-      *        MOVE "N" TO WS-TROUVE 
-      *Comparaison des matières récupérées avec les matières dans le tableau tampon
-      *        PERFORM VARYING WS-IDX-MAT FROM 1 BY 1 
-      *                UNTIL WS-IDX-MAT > WS-NBRE-MAT 
-      *                OR WS-TROUVE = "Y"
-      *           
-      *           IF WS-NOM-MAT-UNIQ(WS-IDX-MAT) = WS-TEMPO 
-      *               MOVE "Y" TO WS-TROUVE 
-      *           END-IF 
-      *
-      *        END-PERFORM
-             
-      *Lorsque pas de matière correspondante trouvée, ajout de la matière dans le tableau tampon  
-      *        IF WS-TROUVE = "N"
-      *           ADD 1 TO WS-NBRE-MAT
-      *           MOVE WS-TEMPO TO WS-NOM-MAT-UNIQ(WS-NBRE-MAT)
-      *        END-IF 
-
-      *     END-IF
-      *    END-PERFORM 
-
-      *END-PERFORM.
-
-      *Affichage des matières récupérées dans le tableau 
-      *PERFORM VARYING WS-IDX-MAT FROM 1 BY 1 
-      *                UNTIL WS-IDX-MAT > WS-NBRE-MAT 
-
-      *        DISPLAY WS-NOM-MAT-UNIQ(WS-IDX-MAT)
-      *END-PERFORM.
-
-
-      *0510-RECUP-MAT-FIN.
-      *EXIT.
-
 
       *-----------------------------------------------------------------
+       
+       0310-MOYENNE-CLASSE-DEBUT.
 
+       MOVE 0 TO WS-SOMME.
+       MOVE 0 TO WS-TOT-COEF.
+       PERFORM VARYING WS-IDX-COURS FROM 1 BY 1 
+               UNTIL WS-IDX-COURS > WS-NBRE-COURS
+           
+
+           COMPUTE WS-MOY-MAT-POND(WS-IDX-ETUD,WS-IDX-COURS) =
+                   WS-COEF(1,WS-IDX-COURS) * 
+                   WS-MOY-MAT(WS-IDX-ETUD,WS-IDX-COURS)
+           
+           
+
+           ADD WS-MOY-MAT-POND(WS-IDX-ETUD,WS-IDX-COURS)
+           TO  WS-SOMME
+      *    DISPLAY WS-SOMME
+           
+           ADD WS-COEF(1,WS-IDX-COURS)
+           TO  WS-TOT-COEF 
+      *    DISPLAY WS-TOT-COEF 
+
+
+       END-PERFORM.
+
+       COMPUTE WS-MOYENNE-CLASSE ROUNDED = WS-SOMME / WS-TOT-COEF.
+                
+
+
+       0310-MOYENNE-CLASSE-FIN.
+       EXIT.
+
+      *-----------------------------------------------------------------
+       0320-AFFICHE-MOY-MAT-DEBUT.
+       
+       DISPLAY "Affichage des moyennes par matière : ".
+       DISPLAY WS-ETOILE.
+       
+       DISPLAY WS-ENTETE-MAT
+       SPACES WITH NO ADVANCING. 
+       DISPLAY WS-ENTETE-MOYENNE.
+
+       DISPLAY WS-ETOILE.
+
+       PERFORM VARYING WS-IDX-COURS FROM 1 BY 1 
+               UNTIL WS-IDX-COURS > WS-NBRE-COURS
+
+
+           DISPLAY WS-MATIERE(1,WS-IDX-COURS)
+           SPACES WITH NO ADVANCING
+           DISPLAY WS-MOY-MAT(WS-IDX-ETUD,WS-IDX-COURS)
+            
+           DISPLAY WS-TIRET
+
+       END-PERFORM.
+
+       DISPLAY WS-ENTETE-MOY-CLS WS-MOYENNE-CLASSE.
+
+       0320-AFFICHE-MOY-MAT-FIN.
+       EXIT. 
